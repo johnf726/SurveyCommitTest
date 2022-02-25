0ed08a599dcde2a379070393a069c17eabe0779a
David Gonzalez <david623@revature.net>
2022-02-25 16:34:53 -0500

    Second Commit

---
+++ FeedBack-Team-D-1/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  FeedBack-Team-D-1
+//
+//  Created by David Gonzalez on 2/25/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Welcome my Friend")
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+}
--- FeedBack-Team-D-1/ViewController.swift
+++ FeedBack-Team-D-1/ViewController.swift
@@ -4,16 +4,40 @@
 //
 //  Created by David Gonzalez on 2/25/22.
 //
-
 import UIKit
-
+ //NotificationBannerSwift
+// simple comment
 class ViewController: UIViewController {
-
+     
+    // MARK: - Outlets
+    @IBOutlet weak var userEmailText: UITextField!
+    
+    @IBOutlet weak var userPasswordText: UITextField!
+    
+    // MARK: - IBActions
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBAction func submitButton2(_ sender: Any) {
+        performLogin()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    private func setupUI() {
+        submitButton.layer.cornerRadius = 20
     }
 
+    private func performLogin() {
+        guard let email = userEmailText.text, !email.isEmpty else {
+            print("Email empty")
+//            NotificationBanner(title: "Error", subtitle: "Debes especificar un correo.", style: .warning).show()
+
+            return
+        }
+    }
 
 }
 
