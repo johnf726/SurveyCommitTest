56db102bc412f733894c941f37fc69d7de4ec2f6
David Gonzalez <david623@revature.net>
2022-03-01 09:58:27 -0500

    Add Project Structure and Add User object Core Data

---
+++ FeedBack-Team-D-1/Model/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  FeedBack-Team-D-1
+//
+//  Created by David Gonzalez on 3/1/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
