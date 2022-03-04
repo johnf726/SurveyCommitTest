fd26d46de6957f66167b5d4e4de495b5148236f8
David Gonzalez <99767944+David-Gonzalez-Revature@users.noreply.github.com>
2022-03-04 14:03:43 -0500

    Add Get Data from Core Data , number of ranking good and bad, for dynamic response

--- FeedBack-Team-D-1/ViewControl/Controller/TnksViewController.swift
+++ FeedBack-Team-D-1/ViewControl/Controller/TnksViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 import AVFoundation
 
 class TnksViewController: UIViewController {
-
+    // MARK: - AVAudioPlayer
     var audioPlayer: AVAudioPlayer!
  //   var audioPlayer : AVAudioPlayer!
     var soundNameSad = "sad"
     var soundNameHappy = "happy"
+    var emailG = "davisgon@gmail.com"
     
     func PlayAudio( sound: String){
         let soundURL = Bundle.main.url(forResource: sound, withExtension: "mp3")
@@ -47,15 +48,7 @@ class TnksViewController: UIViewController {
     
     @IBOutlet weak var S5Selected: UIButton!
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+      
     // MARK: - IBOAction
     @IBAction func S5_Happy(_ sender: Any) {
         sadImage.isHidden = true
@@ -65,7 +58,11 @@ class TnksViewController: UIViewController {
         S1NoSelectedButton.isHidden = false
         S1SelectedButton.isHidden = true
         print("aca")
-        PlayAudio( sound: soundNameHappy)
+        //PlayAudio( sound: soundNameHappy)
+        addDataUserScore(p : 100)
+        getDataUserScore()
+     //   getOneDataUser()
+        getOneDataUserScore( p : emailG)
     }
     
     @IBAction func S5Selected(_ sender: Any) {
@@ -83,9 +80,14 @@ class TnksViewController: UIViewController {
         happyImage.isHidden = true
         S1NoSelectedButton.isHidden = true
         S1SelectedButton.isHidden = false
-        PlayAudio( sound: soundNameSad)
+      //  PlayAudio( sound: soundNameSad)
         S5NoSelected.isHidden = false
         S5Selected.isHidden = true
+        addDataUserScore(p : 60)
+        getDataUserScore()
+        getOneDataUserScore( p : emailG)
+       
+        
     }
     @IBAction func S1Selected(_ sender: Any) {
         print("Selected button 1")
@@ -99,8 +101,8 @@ class TnksViewController: UIViewController {
     @IBAction func recuperaData(_ sender: Any) {
         
        // getOneDataUser()
-        addDataUserScore()
-        getOneDataUserScore()
+       // addDataUserScore()
+        //getOneDataUserScore()
         getDataUserScore()
     }
     
@@ -113,14 +115,14 @@ class TnksViewController: UIViewController {
     }
     
     // MARK: - CORE DATA USER-SCORE
-    func addDataUserScore(){
+    func addDataUserScore(p : Int32){
         let date = Date()
-        CoreDataManage.inst.addDataUserUserScore(emailP: "davisgon@gmail.com", scoreP: 100, dateCreated: date  )
+        CoreDataManage.inst.addDataUserUserScore(emailP: "davisgon@gmail.com", scoreP: Int(p), dateCreated: date  )
         print("Data Saved into USER-SCORE Entity")
     }
     
-    func getOneDataUserScore() {
-        let ds =  CoreDataManage.inst.getOneDataUserScore(n: "davisgon@gmail.com")
+    func getOneDataUserScore(p : String) {
+        let ds =  CoreDataManage.inst.getOneDataUserScore(n: p)
       
         print("GetOneDataUser from USER-STORE ",
               ds.email,
@@ -131,9 +133,20 @@ class TnksViewController: UIViewController {
     
     func getDataUserScore() {
         var data = CoreDataManage.inst.getDataUserScore() //.getData()
+        var good = 0
+        var bad  = 0
+        var  numRatingTotal  = CoreDataManage.inst.getDataUserScore().count
         for d in data{
+            if d.score == 100{
+                var numRating = CoreDataManage.inst.getDataUserScore().count
+                good += 1
+            }else{
+                var numRating = CoreDataManage.inst.getDataUserScore().count
+                bad += 1
+            }
             print("Email is ",d.email,"Score ", d.score, "DateCreated USER SCORE ",d.dateCreated)
         }
+        print(numRatingTotal, "Total Bad:",bad," Total Good: ",good)
         
         
     }
